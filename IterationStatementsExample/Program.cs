da907349488a8610b834a06198168a498c511154
Seth Bowman <brandonsethbowman@gmail.com>
2023-07-26 10:31:33 -0500

    Add project files.

---
+++ IterationStatementsExample/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace IterationStatementsExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //While loop
+            Console.WriteLine("WHILE LOOP:");
+            var cont = "yes";
+            while (cont == "yes")
+            {
+                Console.WriteLine("Continue? (Type 'yes' or 'no')");
+                cont = Console.ReadLine();
+
+            }
+
+            Console.WriteLine("Outside of the loop");
+            Console.WriteLine("Press enter.");
+            Console.ReadKey();
+
+            //Do While loop
+            Console.WriteLine("DO WHILE LOOP");
+            var num = 1;
+
+            do
+            {
+                Console.WriteLine(num);
+                num++;
+            } while (num <= 5);
+
+            Console.WriteLine("Press enter.");
+            Console.ReadKey();
+
+            //For loop
+            Console.WriteLine("FOR LOOP");
+
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{i} is the number we are on");
+            }
+
+            Console.WriteLine("Press enter.");
+            Console.ReadKey();
+
+            //Foreach loop
+            Console.WriteLine("FOREACH LOOP");
+            var numbers = new int[] { 12, 55, 900, 30, 2 };
+
+            foreach(var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
